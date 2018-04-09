a78bc283332942247c44518dcc85699b44ab83eb
Christian Brevik <christian.brevik@gmail.com>
2018-04-09 13:04:33 +0200

    Add simple config for lines moved

--- extension.js
+++ extension.js
@@ -1,15 +1,40 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 function activate(context) {
-    const disposableScrollWithCursorUp = vscode.commands.registerCommand('extension.scrollWithCursorUp', function () {
-        vscode.commands.executeCommand("cursorUp");
-        vscode.commands.executeCommand("scrollLineUp");
-    });
-    const disposableScrollWithCursorDown = vscode.commands.registerCommand('extension.scrollWithCursorDown', function () {
-        vscode.commands.executeCommand("cursorDown");
-        vscode.commands.executeCommand("scrollLineDown");
-    });
-    context.subscriptions = [disposableScrollWithCursorUp, disposableScrollWithCursorDown];
+  const config = vscode.workspace.getConfiguration("scrollWithCursor");
+  const moveLinesUp = config.get("moveLinesUp", 1);
+  const moveLinesDown = config.get("moveLinesDown", 1);
+
+  const moveUpArgs = {
+    to: "up",
+    by: "line",
+    value: moveLinesUp
+  };
+
+  const moveDownArgs = {
+    to: "down",
+    by: "line",
+    value: moveLinesDown
+  };
+
+  const disposableScrollWithCursorUp = vscode.commands.registerCommand(
+    "extension.scrollWithCursorUp",
+    function() {
+      vscode.commands.executeCommand("cursorMove", moveUpArgs);
+      vscode.commands.executeCommand("editorScroll", moveUpArgs);
+    }
+  );
+  const disposableScrollWithCursorDown = vscode.commands.registerCommand(
+    "extension.scrollWithCursorDown",
+    function() {
+      vscode.commands.executeCommand("cursorMove", moveDownArgs);
+      vscode.commands.executeCommand("editorScroll", moveDownArgs);
+    }
+  );
+  context.subscriptions = [
+    disposableScrollWithCursorUp,
+    disposableScrollWithCursorDown
+  ];
 }
 
 exports.activate = activate;
